650f0bac874379909571ca67e82720a8199fe9b6
Angela Webb <angelawebb.dev@gmail.com>
2024-04-15 20:07:35 -0400

    adds alternative items to display in mylist.jsx

--- client/src/components/myList.jsx
+++ client/src/components/myList.jsx
@@ -20,25 +20,30 @@ const Mylist = (props) => {
         <table>
             <thead>
                 <tr>
-                    <th></th>
+                    <th>&#x2714;</th>
                     <th>Item</th>
-                    <th>Section</th>
                     <th>Notes</th>
+                    <th>Alternatives</th>
+                    <th>Section</th>
                     <th></th>
                 </tr>
             </thead>
             <tbody>
             {itemlist.sort((item1, item2) => (item1.section.toLowerCase() < item2.section.toLowerCase() ? -1 : ((item1.section.toLowerCase() > item2.section.toLowerCase()) ? 1 : 0))).map(item => { 
                 return (
+                    <>
                     <tr key={item._id}>
                         <td><input type='checkbox'  onChange={e => deleteItem(item._id, item.name)}  style={{border:'1px solid blue', margin:'3px', padding:'5px', borderRadius:'2px'}}/></td>
                         <td><Link to={`/details/${item._id}`} >{item.name}</Link></td>
-                        <td><p>{item.section}</p></td>
                         <td><p>{item.notes}</p></td>
+                        {item.alternative1 || item.alternative2 ? <td>{item.alternative1} {item.alternative2}</td>
+                                                                : <td></td>}
+                        <td><p>{item.section}</p></td>
                         <td>
                             <Link to={`/edit/${item._id}`}>update</Link>
                         </td>
                     </tr>
+                    </>
                 )
             })}
             </tbody>
