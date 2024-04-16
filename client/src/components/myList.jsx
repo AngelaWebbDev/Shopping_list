b463bacaa6477e26df5f8fd9e985b24a4890e664
Angela Webb <angelawebb.dev@gmail.com>
2024-04-16 06:10:43 -0400

    removes <></> to correct key id error

--- client/src/components/myList.jsx
+++ client/src/components/myList.jsx
@@ -110,7 +110,6 @@ const Mylist = (props) => {
             <tbody>
             {itemlist.sort((item1, item2) => (item1.section.toLowerCase() < item2.section.toLowerCase() ? -1 : ((item1.section.toLowerCase() > item2.section.toLowerCase()) ? 1 : 0))).map(item => { 
                 return (
-                    <>
                     <tr key={item._id}>
                         <td><input type='checkbox'  onChange={e => deleteItem(item._id, item.name)}  style={{border:'1px solid blue', margin:'3px', padding:'5px', borderRadius:'2px'}}/></td>
                         <td><Link to={`/details/${item._id}`} >{item.name}</Link></td>
@@ -122,7 +121,6 @@ const Mylist = (props) => {
                             <Link to={`/edit/${item._id}`}>&#128393;</Link>
                         </td>
                     </tr>
-                    </>
                 )
             })}
             </tbody>
