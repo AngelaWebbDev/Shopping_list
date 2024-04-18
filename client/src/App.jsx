d2b9f792a1108314abd3ce6bef4fcc74422e8d12
Angela Webb <angelawebb.dev@gmail.com>
2024-04-18 07:48:00 -0400

    moves getAll api call to mylist.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,25 +1,17 @@
-import { useEffect, useState } from 'react'
+
 import './App.css'
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import Mylist from './components/myList'
-import axios from 'axios'
 import Edititem from './components/editItem'
 
 function App() {
-  const [itemlist, setItemlist] = useState([])
-
-  useEffect(() => {
-    axios.get('http://localhost:8000/api/getAll')
-        .then(res => { setItemlist(res.data) })
-        .catch(err => console.log('appjsx getAll err: ', err))
-  }, [])
 
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path='/' element={<Mylist itemlist={itemlist} setItemlist={setItemlist}/>}/>
-          <Route path='/edit/:id' element={<Edititem itemlist={itemlist} setItemlist={setItemlist}/>} />
+          <Route path='/' element={<Mylist/>}/>
+          <Route path='/edit/:id' element={<Edititem/>} />
         </Routes>
       </BrowserRouter>
     </>
