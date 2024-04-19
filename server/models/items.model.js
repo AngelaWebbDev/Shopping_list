4439b30457ae0f9f8a4a39c1caff23981721ca97
Angela Webb <angelawebb.dev@gmail.com>
2024-04-19 10:53:20 -0400

    sets maxlength for alternates

--- server/models/items.model.js
+++ server/models/items.model.js
@@ -3,12 +3,15 @@ const mongoose = require('mongoose');
 const ItemsSchema = mongoose.Schema({
     name:{type:String,
                 required:[true,'Item name is required.'],
-                minlength:[2,'Item name must be at least 2 characters long.']},
+                minlength:[2,'Item name must be at least 2 characters long.'],
+                maxlength:[20, 'Item name cannot be more than 20 characters long.']},
     section:{type:String,
-            required:[true,'Grocery section is required.']},
+            required:[true,'section is required.']},
     notes:{type:String},
-    alternative1:{type:String},
-    alternative2:{type:String}
+    alternative1:{type:String,
+        maxlength:[20, 'Item name cannot be more than 20 characters long.']},
+    alternative2:{type:String,
+        maxlength:[20, 'Item name cannot be more than 20 characters long.']}
 }, {timestamps:true})
 
 module.exports = mongoose.model('Items', ItemsSchema)
