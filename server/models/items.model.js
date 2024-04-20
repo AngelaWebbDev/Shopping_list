1a78d89821998234652257001853440eb123c167
Angela Webb <angelawebb.dev@gmail.com>
2024-04-20 12:01:39 -0400

    adds length restrictions to section

--- server/models/items.model.js
+++ server/models/items.model.js
@@ -2,16 +2,18 @@ const mongoose = require('mongoose');
 
 const ItemsSchema = mongoose.Schema({
     name:{type:String,
-                required:[true,'Item name is required.'],
-                minlength:[2,'Item name must be at least 2 characters long.'],
-                maxlength:[20, 'Item name cannot be more than 20 characters long.']},
+            required:[true,'Item name required.'],
+            minlength:[2,'Item name must be at least 2 characters long.'],
+            maxlength:[20, 'Item name cannot be more than 20 characters long.']},
     section:{type:String,
-            required:[true,'section is required.']},
+                required:[true,'section is required.'],
+                minlength:[1,'section is required'],
+                maxlength:[10, 'Section cannot be more than 10 characters long.']},
     notes:{type:String},
     alternative1:{type:String,
-        maxlength:[20, 'Item name cannot be more than 20 characters long.']},
+                    maxlength:[20, 'Item name cannot be more than 20 characters long.']},
     alternative2:{type:String,
-        maxlength:[20, 'Item name cannot be more than 20 characters long.']}
+                    maxlength:[20, 'Item name cannot be more than 20 characters long.']}
 }, {timestamps:true})
 
 module.exports = mongoose.model('Items', ItemsSchema)
